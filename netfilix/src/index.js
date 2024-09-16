4e41556ee0590f2ccf7912776db339307a329284
Jayant kumar <jayantpaswan01@gmail.com>
2024-09-16 05:48:49 +0000

    container config

---
+++ netfilix/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <h1>Paytm Cricket Series</h1>
+    <p>Cricket series match countdown.......</p>
+    <ol>
+      <li>ODI 29 December 2024</li>
+      <li>ODI 2 January 2025</li>
+      <li>ODI 5 January 2025</li>
+      <li>ODI 8 January 2025</li>
+      <li>ODI 12 January 2025</li>
+    </ol>
+  </>                 
+    
+  
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
